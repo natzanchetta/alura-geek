a3325a25f0b8bf1763ce10aad0b43b3dda5c70d4
Natália Zanchetta <nataliazanchetta@hotmail.com>
2024-06-05 23:01:57 -0300

    third commit

--- js/criarProduto.js
+++ js/criarProduto.js
@@ -2,17 +2,21 @@ import { conectaApi } from "./conectaApi.js";
 
 const formulario = document.querySelector("[data-formulario]");
 
+
 async function criarProduto(evento) {
     evento.preventDefault();
 
     const imagem = document.querySelector("[data-imagem]").value;
     const nome = document.querySelector("[data-nome]").value;
     const valor = document.querySelector("[data-valor]").value;
-
-    await conectaApi.criaProdutos(imagem, nome, valor);
-
-    //window.location.href = "../pages/envio-concluido.html"
-    alert("Produto enviado com sucesso!");
+    try {
+        await conectaApi.criaProdutos(imagem, nome, valor);
+        
+        //window.location.href = "../pages/envio-concluido.html"
+        alert("Produto enviado com sucesso!");
+    } catch (e) {
+        alert(`Erro ao enviar produto: ${e.message}`);
+    }
 }
 
 formulario.addEventListener("submit", evento => criarProduto(evento));
